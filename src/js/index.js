b6c2777c2212deb2a002881d4ee9ab153771d94a
Victor Martins Dias <victormartinsjob@gmail.com>
2026-04-15 21:23:28 -0300

    feat(list): capture input text and trigger add action on Enter

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,25 @@
 */
 
 import { clamp, escapeHTML, formatCurrencyBRL, formatDate, generateId } from "./utils.js";
+
+const input = document.querySelector("#item");
+const btnAddItem = document.querySelector(".btn-add-item");
+
+btnAddItem.addEventListener("click", () => {
+  const text = input.value.trim();
+
+  if (text !== "") {
+    console.log("Texto capturado:", text);
+
+    input.value = "";
+    input.focus();
+  } else {
+    alert("Por favor, digite algo!");
+  }
+});
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    btnAddItem.click();
+  }
+});
